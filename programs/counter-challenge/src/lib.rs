c4c66d02a4a95fdf5174bb3f52c421fb0a87ad45
Diego <britodiego.tech@gmail.com>
2025-02-01 17:36:22 -0400

    local program development

--- programs/counter-challenge/src/lib.rs
+++ programs/counter-challenge/src/lib.rs
@@ -3,14 +3,61 @@ use anchor_lang::prelude::*;
 declare_id!("3EjBmyZf3Z3uiSpHJQRzJnae42q7qcwuHEQ6CKWRXbKb");
 
 #[program]
-pub mod counter_challenge {
+pub mod counter_program {
     use super::*;
-
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter =&mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter has been initializated {}", counter.count);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        let counter =&mut ctx.accounts.counter;
+        msg!("Counter before increment {}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter after increment {}", counter.count);
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Update>) -> Result<()> {
+        let counter =&mut ctx.accounts.counter;
+        msg!("Counter before decrement {}", counter.count);
+
+        if counter.count == 0 {
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
+        counter.count = counter.count.checked_sub(1).unwrap();
+        msg!("Counter after decrement {}", counter.count);
         Ok(())
     }
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+  pub count: u64
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR + Counter::INIT_SPACE
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+const ANCHOR_DISCRIMINATOR: usize = 8;
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>
+}
--- tests/counter-challenge.ts
+++ tests/counter-challenge.ts
@@ -1,16 +1,53 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { CounterChallenge } from "../target/types/counter_challenge";
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { expect } from "chai"
+import { CounterProgram } from "../target/types/counter_program"
 
-describe("counter-challenge", () => {
+describe("anchor-counter", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
-  const program = anchor.workspace.CounterChallenge as Program<CounterChallenge>;
+  const program = anchor.workspace.CounterProgram as Program<CounterProgram>
+
+  const counter = anchor.web3.Keypair.generate()
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    const tx = await program.methods
+      .initialize()
+      .accounts({ 
+        counter: counter.publicKey 
+      })
+      .signers([counter])
+      .rpc()
+
+    const account = await program.account["counter"].fetch(counter.publicKey)
+    expect(account.count.toNumber() === 0)
+  })
+
+  it("Incremented the count", async () => {
+    const tx = await program.methods
+      .increment()
+      .accounts({
+        counter: counter.publicKey,
+        user: provider.wallet.publicKey 
+      })
+      .rpc()
+
+    const account = await program.account["counter"].fetch(counter.publicKey)
+    expect(account.count.toNumber() === 1)
+  })
+
+  it("Decremented the count", async () => {
+    const tx = await program.methods
+      .decrement()
+      .accounts({ 
+        counter: counter.publicKey 
+      })
+      .rpc()
+
+    const account = await program.account["counter"].fetch(counter.publicKey)
+    expect(account.count.toNumber() === 0)
+  })
+})
